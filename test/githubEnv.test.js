f86d2e9e62a6b7459d5c4e70a45bff64ca9f35f4
Shun Iwashita <iwashunabc519@gmail.com>
2025-07-01 10:02:19 +0700

    update

--- test/githubEnv.test.js
+++ test/githubEnv.test.js
@@ -6,4 +6,8 @@ describe('Load github environment variables', () => {
   test('Get TEST_CODE', () => {
     expect(process.env.TEST_CODE).toBe('hello1234');
   })
+
+  test('SECRET_KEY is not undefined', () => {
+    expect(process.env.SECRET_KEY).toBeDefined();
+  })
 });

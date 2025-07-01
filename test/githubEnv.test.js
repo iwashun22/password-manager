f3bb3c7e54987cc6b28b4152e75ff188240ecb2f
Shun Iwashita <iwashunabc519@gmail.com>
2025-07-01 09:59:46 +0700

    update

--- test/githubEnv.test.js
+++ test/githubEnv.test.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
 describe('Load github environment variables', () => {
   test('Get TEST_CODE', () => {
     expect(process.env.TEST_CODE).toBe('hello1234');
